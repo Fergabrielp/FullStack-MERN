65a369a289863fe2210e317821142255c010fe0e
Your Name <perezfernando95@gmail.com>
2022-06-21 11:28:36 -0300

    Environment Variables

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import "./database/connectDB.js";
+import authRouter from "./routes/auth.route.js";
+
 const app = express();
-app.get("/", (req, res) => {
-  res.json({ ok: true });
-});
+
+app.use("/", authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("http://localhost:" + PORT));
